bef3c937ff5e67a580b6418f30cf26a25dd7e5fd
Noémie Letenneur <n.letenneur@gmail.com>
2024-05-22 17:11:27 +0200

    TP03

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,20 @@
 package dev;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.ResourceBundle;
+import com.github.lalyos.jfiglet.FigletFont;
+
+public class App {
+	public static void main(String[] args) {
+		try {
+			String titre = ResourceBundle.getBundle("application").getString("titre");
+			String asciiArt = FigletFont.convertOneLine(titre);
+			System.out.println(asciiArt);
+			// affichage de l'environnement
+			String environnement = ResourceBundle.getBundle("application").getString("environnement");
+			System.out.println("Environnement : " + environnement);
+		} catch (Exception e) {
+
+		}
+
+	}
 }
